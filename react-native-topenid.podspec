ad85d6792b8cf4466c51e960a3cf2405318f8563
Tung Nguyen <tung.nguyen.it0201@gmail.com>
2022-05-17 15:14:55 +0700

    test update

---
+++ react-native-topenid.podspec
@@ -0,0 +1,18 @@
+require 'json'
+
+package = JSON.parse(File.read(File.join(__dir__, 'package.json')))
+
+Pod::Spec.new do |s|
+  s.name         = package['name']
+  s.version      = package['version']
+  s.summary      = package['description']
+  s.license      = package['license']
+
+  s.authors      = package['author']
+  s.homepage     = package['homepage']
+  s.platforms    = { :ios => "9.0", :osx => "10.13" }
+
+  s.source       = { :git => "https://github.com/tungnguyenit0201/react-native-topenid.gitt", :tag => "v#{s.version}" }
+
+  s.dependency 'React-Core'
+end
--- src/components/Loading.js
+++ src/components/Loading.js
@@ -8,17 +8,19 @@ import Svg, {
   RadialGradient,
   Stop,
 } from 'react-native-svg';
-import {Animated} from 'react-native';
+import {Animated, Easing} from 'react-native';
 
 function Loading(props) {
   const spinValue = useRef(new Animated.Value(0)).current;
   useEffect(() => {
-    Animated.timing(spinValue, {
-      toValue: 1,
-      duration: 3000,
-      easing: Easing.linear, // Easing is an additional import from react-native
-      useNativeDriver: true, // To make use of native driver for performance
-    }).start();
+    Animated.loop(
+      Animated.timing(spinValue, {
+        toValue: 1,
+        duration: 1000,
+        easing: Easing.linear,
+        useNativeDriver: true,
+      }),
+    ).start();
   }, []);
 
   const spin = spinValue.interpolate({
@@ -26,48 +28,10 @@ function Loading(props) {
     outputRange: ['0deg', '360deg'],
   });
   return (
-    <Svg
-      width={120}
-      height={120}
-      viewBox="0 0 120 120"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      {...props}>
-      <Mask
-        id="a"
-        style={{
-          maskType: 'alpha',
-        }}
-        maskUnits="userSpaceOnUse"
-        x={0}
-        y={0}
-        width={120}
-        height={120}>
-        <Path
-          d="M120 60c0 33.137-26.863 60-60 60S0 93.137 0 60 26.863 0 60 0s60 26.863 60 60zm-98.87 0c0 21.467 17.403 38.87 38.87 38.87S98.87 81.467 98.87 60 81.467 21.13 60 21.13 21.13 38.533 21.13 60z"
-          fill="#C4C4C4"
-        />
-      </Mask>
-      <G mask="url(#a)">
-        <Path
-          fill="url(#paint0_angular_24_8787)"
-          d="M-7.88367 -7.88306H127.88233V127.88293999999999H-7.88367z"
-        />
-        <Circle cx={60.81} cy={109.848} r={10.7397} fill="#EB7A23" />
-      </G>
-      <Defs>
-        <RadialGradient
-          id="paint0_angular_24_8787"
-          cx={0}
-          cy={0}
-          r={1}
-          gradientUnits="userSpaceOnUse"
-          gradientTransform="rotate(90 0 60) scale(67.8832)">
-          <Stop stopColor="#EB7A23" />
-          <Stop offset={1} stopColor="#EB7A23" stopOpacity={0} />
-        </RadialGradient>
-      </Defs>
-    </Svg>
+    <Animated.Image
+      style={{transform: [{rotate: spin}], width: 40, height: 40}}
+      source={require('../images/loading.png')}
+    />
   );
 }
 
